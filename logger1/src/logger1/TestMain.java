7358521f90689a7fc9e8a40371c0666610c2c8cd
zhuzhuzhulc <770116831@qq.com>
2017-02-02 20:36:37 -0500

    ada
    ada

--- logger1/src/logger1/TestMain.java
+++ logger1/src/logger1/TestMain.java
@@ -1,24 +1,31 @@
 package logger1;
 
 import java.io.BufferedReader;
+import java.io.IOException;
 import java.io.InputStreamReader;
-import java.util.Queue;
+import java.util.*;
 
 public class TestMain {
 	public static void main(String args[]){
-
-	    Logger log = new Logger();
+		String type = args[0];
+	    Logger log = new Logger(type);
 	    while (true){
 	        InputStreamReader isrd = new InputStreamReader(System.in);
 	        BufferedReader br = new BufferedReader(isrd);
-	        String input = br.readLine();
+	        String input = null;
+			try {
+				input = br.readLine();
+			} catch (IOException e) {
+				// TODO Auto-generated catch block
+				e.printStackTrace();
+			}
 	        if (input.equals("p")){
 	        	log.print();
 	        }
 	    }
 	}
 	private class LogListen implements Runnable{
-		Queue<TimeStampedMessage> queue = 
+		Queue<TimeStampedMessage> queue = new LinkedList<TimeStampedMessage>();
 		
 	
 		@Override
