6e9434d7daf30293baa0a154b449cd007cb1717d
zhuzhuzhulc <770116831@qq.com>
2017-02-02 20:43:23 -0500

    Merge branch 'master' of https://github.com/yanjunlin1994/dslab1
    # Conflicts:
    #	logger1/bin/logger1/TestMain.class
    #	logger1/src/logger1/TestMain.java

--- logger1/src/logger1/TestMain.java
+++ logger1/src/logger1/TestMain.java
@@ -7,43 +7,26 @@ import java.util.*;
 
 public class TestMain {
 	public static void main(String args[]){
-<<<<<<< HEAD
-		String type = args[0];
-	    Logger log = new Logger(type);
-	    while (true){
-	        InputStreamReader isrd = new InputStreamReader(System.in);
-	        BufferedReader br = new BufferedReader(isrd);
-	        String input = null;
-			try {
-				input = br.readLine();
-			} catch (IOException e) {
-				// TODO Auto-generated catch block
-				e.printStackTrace();
-			}
-=======
-	    Logger log = new Logger();
+		String clockType = args[0];
+	    Logger log = new Logger(clockType);
 	    while (true){
 	        InputStreamReader isrd = new InputStreamReader(System.in);
 	        BufferedReader br = new BufferedReader(isrd);
 	        String input = br.readLine();
 	        try {
->>>>>>> origin/master
+
 	        if (input.equals("p")){
 	        	log.print();
 	        }
-	    } catch(Exception e) {
-            e.printStackTrace();
-        } 
+		    } catch(Exception e) {
+	            e.printStackTrace();
+	        } 
 	        
 	    }
 	    
 	}
-<<<<<<< HEAD
-	private class LogListen implements Runnable{
-		Queue<TimeStampedMessage> queue = new LinkedList<TimeStampedMessage>();
-		
-=======
->>>>>>> origin/master
+
+
 	
 	
 	
