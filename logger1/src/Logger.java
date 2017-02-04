08dbff4ffe258fdcb86b2eae939429f5c80f5fe4
yanjunlin1994 <yanjunl1@andrew.cmu.edu>
2017-02-03 21:10:24 -0500

    finishgit add ./git add ./

--- logger1/src/Logger.java
+++ logger1/src/Logger.java
@@ -1,5 +1,8 @@
 
 import java.io.BufferedReader;
+import java.io.BufferedWriter;
+import java.io.FileWriter;
+import java.io.IOException;
 import java.io.InputStreamReader;
 import java.util.*;
 public class Logger {
@@ -32,41 +35,71 @@ public class Logger {
         }  
     }
     public synchronized void print(){
+        String FILENAME = "./logfile.txt";
+        BufferedWriter bw = null;
+        FileWriter fw = null;
+        
         while (!this.messageQueue.isEmpty()){   
             this.messageList.add(this.messageQueue.poll());
         }
         if (messageList.size() > 0) {
             Collections.sort(messageList, new CompareByTimeStamp());
-            //TODO:CLOCK TYPE
-            for (int i = 0; i<messageList.size()-1;i++){	
-            	for (int j = i+1;j<messageList.size();j++){
-            		if (isConcurrent(clockType, messageList.get(i), messageList.get(j))){
-            			System.out.println(">>>>>> These two message are concurrent>>>>");
-            			System.out.println(messageList.get(i));
-            			System.out.println(messageList.get(j));
-            			System.out.println("<<<<<<<<<<<<<<<<<<<<<<<<<<<<<<<<<<<<<<<<<<<<");
-            		}
-            	}
+            try {
+                fw = new FileWriter(FILENAME);
+                bw = new BufferedWriter(fw);
+                for (int i = 0; i<messageList.size()-1;i++){    
+                    for (int j = i+1;j<messageList.size();j++){
+                        if (isConcurrent(clockType, messageList.get(i), messageList.get(j))){
+                            bw.write(">>>>>> These two message are concurrent>>>>\n");
+                            bw.write(messageList.get(i).toString());
+                            bw.newLine();
+                            bw.write(messageList.get(j).toString());
+                            bw.newLine();
+                            bw.write("<<<<<<<<<<<<<<<<<<<<<<<<<<<<<<<<<<<<<<<<<<<<\n");
+                        }
+                    }
+                }
+                int[] Mark = new int[messageList.size()];
+                for (int i = 0; i<messageList.size();i++){
+                    if(Mark[i]!=1){
+                        ArrayList<TimeStampedMessage> seqList = new ArrayList<TimeStampedMessage>();
+                        seqList.add(messageList.get(i));
+                        for (int j = i + 1;j<messageList.size();j++){
+                            if (!isConcurrent(clockType, seqList.get(seqList.size()-1),messageList.get(j))){
+                                seqList.add(messageList.get(j));
+                                Mark[j] = 1;
+                            }
+                        }
+                        bw.write(">>>>> These messages are sequential>>>>>>>>\n");
+                        for (TimeStampedMessage msg : seqList){
+                            bw.write(msg.toString());
+                            bw.newLine();
+                        }
+                        //Arrays.fill(Mark, 0);
+                        bw.write("<<<<<<<<<<<<<<<<<<<<<<<<<<<<<<<<<<<<<<<<<<<<\n");
+                    }
+                }     
+            }  catch(Exception e) {
+
+                e.printStackTrace();
+
+            } finally {
+                try {
+
+                    if (bw != null)
+                        bw.close();
+
+                    if (fw != null)
+                        fw.close();
+
+                } catch (IOException ex) {
+
+                    ex.printStackTrace();
+
+                }
+                
             }
-            int[] Mark = new int[messageList.size()];
-            for (int i = 0; i<messageList.size();i++){
-            	if(Mark[i]!=1){
-            		ArrayList<TimeStampedMessage> seqList = new ArrayList<TimeStampedMessage>();
-            		seqList.add(messageList.get(i));
-            		for (int j = i + 1;j<messageList.size();j++){
-            			if (!isConcurrent(clockType, seqList.get(seqList.size()-1),messageList.get(j))){
-            				seqList.add(messageList.get(j));
-            				Mark[j] = 1;
-            			}
-            		}
-            		System.out.println(">>>>> These messages are sequential>>>>>>>>");
-            		for (TimeStampedMessage msg : seqList){
-            			System.out.println(msg);
-            		}
-            		//Arrays.fill(Mark, 0);
-            		System.out.println("<<<<<<<<<<<<<<<<<<<<<<<<<<<<<<<<<<<<<<<<<<<<");
-            	}
-            }
+            
             
             /*
             if (!ifbefC) {
@@ -82,6 +115,7 @@ public class Logger {
 //            }
 //            System.out.println("---------log's sorted file list end----------");
         }
+        System.out.println("write to file succeed");
         
     }
     private boolean isConcurrent(String clockt, TimeStampedMessage t1, TimeStampedMessage t2) {
@@ -89,7 +123,7 @@ public class Logger {
             int[] m1 = t1.getTimeStamps();
             int[] m2 = t2.getTimeStamps();
             int flag = 0;
-            for (int i = 0; i < m1.length - 1; i++) {
+            for (int i = 0; i < m1.length; i++) {
                 if(flag == 1 && m1[i] < m2[i]) {
                     return true;
                 }
